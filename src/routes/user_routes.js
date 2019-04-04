a46b3255f07c1524c49484dfcb35b64a770b5b72
Mhammad1704 <natgeo1416@hotmail.com>
2019-04-04 19:19:12 +0300

    build

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -39,17 +39,58 @@ router.get('/user/:id', (req, res) => {
  
 router.post("/sign-up", (req, res, next) => {
   // start a promise chain, so that any errors will pass to `handle`
-  Promise.resolve(req.body.credentials)
-    .then(credentials => {
-      if (
-        !credentials ||
-        !credentials.password ||
-        credentials.password !== credentials.password_confirmation
-      ) {
-        throw new BadParamsError();
-      } else {
-        console.log(credentials)
-        return models.User.create( {
+  // Promise.resolve(req.body.credentials)
+  //   .then(credentials => {
+  //     if (
+  //       !credentials ||
+  //       !credentials.password ||
+  //       credentials.password !== credentials.password_confirmation
+  //     ) {
+  //       throw new BadParamsError();
+  //     } else {
+  //       console.log(credentials)
+        // return models.User.create( {
+        //   email: credentials.email,
+        //   hashedPassword: credentials.password,
+        //   // password_confirmation:credentials.password_confirmation,
+        //   // car_pic:credentials.car_pic,
+        //   // additional_info:credentials.additional_info,
+        //   // phone_number:credentials.phone_number
+        //   // name: credentials.name,
+        //   // car_pic: credentials.car_pic ,
+        //   // additional_info: credentials.additional_info ,
+        //   // phone_number: credentials.phone_number
+        //  }
+        // );
+  //     }
+  //   })
+    // .then(user => {
+    //   const payload = {
+    //     id: user.id,
+    //     email: user.email,
+    //     expires: process.env.JWT_EXPIRATION_D + "d"
+    //   };
+
+  //     // assigns payload to req.user
+  //     req.login(payload, { session: false }, error => {
+  //       if (error) {
+  //         next();
+  //       }
+
+  //       // generate a signed json web token and return it in the response
+  //       const token = jwt.sign(JSON.stringify(payload), process.env.PASS_KEY);
+
+  //       // assign our jwt to the cookie
+        // res
+        //   .cookie("jwt", token, { httpOnly: true, secure: false })
+        //   .status(201)
+        //   .json({ id: req.user.id, email: req.user.email });
+  //     });
+  //   })
+  //   // pass any errors along to the error handler
+  //   .catch(e => next());
+
+  models.User.create( {
           email: credentials.email,
           hashedPassword: credentials.password,
           // password_confirmation:credentials.password_confirmation,
@@ -60,37 +101,8 @@ router.post("/sign-up", (req, res, next) => {
           // car_pic: credentials.car_pic ,
           // additional_info: credentials.additional_info ,
           // phone_number: credentials.phone_number
-         }
-        );
-      }
-    })
-    .then(user => {
-      const payload = {
-        id: user.id,
-        email: user.email,
-        expires: process.env.JWT_EXPIRATION_D + "d"
-      };
-
-      // assigns payload to req.user
-      req.login(payload, { session: false }, error => {
-        if (error) {
-          next();
-        }
-
-        // generate a signed json web token and return it in the response
-        const token = jwt.sign(JSON.stringify(payload), process.env.PASS_KEY);
-
-        // assign our jwt to the cookie
-        res
-          .cookie("jwt", token, { httpOnly: true, secure: false })
-          .status(201)
-          .json({ id: req.user.id, email: req.user.email });
-      });
-    })
-    // pass any errors along to the error handler
-    .catch(e => next());
-});
-
+         }).then(user => { res.json({ user})}).catch(e => next());
+})
 router.post("/sign-in", localAuth, (req, res, next) => {
   if (req.user) {
     // This is what ends up in our JWT
